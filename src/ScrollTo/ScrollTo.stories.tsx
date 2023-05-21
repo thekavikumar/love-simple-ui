a5c98949eb5a666d8d023d34a7e0530ee013b889
Deepanshu <21bcs072@iiitdmj.ac.in>
2023-05-21 18:13:45 +0530

    Added scroll to top and scroll up functioality

---
+++ src/ScrollTo/ScrollTo.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {GoToTop,GoToBottom} from "./index";
+
+export const scrollTop = () => (
+    <GoToTop />
+);
+
+export const scrollDown = () => (
+    <GoToBottom />
+);
