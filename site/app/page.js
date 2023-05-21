079f33ea951531f4d66fbc15f2f1f6500b1cbba9
thekavikumar <kavikumarceo@gmail.com>
2023-05-21 20:48:04 +0530

    docs website

---
+++ site/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl text-center">Website Will Up Soon...</h1>
+    </main>
+  );
+}
