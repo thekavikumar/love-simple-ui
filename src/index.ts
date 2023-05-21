b7cf3d1a74a9bbe0b62864967afe127768ffb647
Deepanshu <21bcs072@iiitdmj.ac.in>
2023-05-21 18:34:05 +0530

    Added change to main index.tsx

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { PrimaryButton, LoadingButton, Button3, Button4 } from "./button";
 import { Alert } from "./Alert";
 import { SearchBar } from "./searchBar";
+import { GoToBottom,GoToTop } from "./ScrollTo";
 
-export { PrimaryButton, LoadingButton, Button3, Button4 , Alert, SearchBar};
+export { PrimaryButton, LoadingButton, Button3, Button4 , Alert, SearchBar, GoToBottom,GoToTop};
