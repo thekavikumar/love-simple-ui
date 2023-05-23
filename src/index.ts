853030d02bb685b5c3190d973b11fb1059e249bf
Aditi <adithakurti@gmail.com>
2023-05-23 13:26:32 +0530

    Button Error solved

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { PrimaryButton, LoadingButton, Button3, Button4 } from "./button";
+import { PrimaryButton,  Button3, Button4 } from "./button";
 import { Alert } from "./Alert";
 import { SearchBar } from "./searchBar";
 
-export { PrimaryButton, LoadingButton, Button3, Button4 , Alert, SearchBar};
+export { PrimaryButton,  Button3, Button4 , Alert, SearchBar};
