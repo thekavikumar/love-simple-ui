aeb0d6875100854e8ac4b994608e458af3dba466
Naveen G <naveeng2402@gmail.com>
2023-05-24 22:10:54 +0530

    Adopt the monorepo architecture

---
+++ packages/react/src/index.ts
@@ -0,0 +1,5 @@
+import { PrimaryButton, Button3, Button4 } from "./button";
+import { Alert } from "./Alert";
+import { SearchBar } from "./searchBar";
+
+export { PrimaryButton, Button3, Button4, Alert, SearchBar };
