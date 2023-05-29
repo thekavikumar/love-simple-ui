ef262ac08467cfbecb701275ff63241e39e795ec
Sameep002 <108282267+Sameep002@users.noreply.github.com>
2023-05-29 22:11:59 +0530

    Added accordion component

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { PrimaryButton, Button3, Button4 } from "./button";
 import { Alert } from "./Alert";
 import { SearchBar } from "./searchBar";
 import { Input } from "./input";
+import { Accordion } from "./accordion";
 
-export { PrimaryButton, Button3, Button4 , Alert, SearchBar, Input};
+export { PrimaryButton, Button3, Button4 , Alert, SearchBar, Input, Accordion};
