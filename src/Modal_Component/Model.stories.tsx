9a8aaa1987dfd6edb0f9efa84d9606ca6a4a4495
Bhavya Patel <100468926+BhavyaMPatel@users.noreply.github.com>
2023-06-13 19:12:15 +0530

    Update Model.stories.tsx
    Change Modal Name

--- src/Modal_Component/Model.stories.tsx
+++ src/Modal_Component/Model.stories.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { SmallMessageModel } from "./index";
 
 export const Primary_Model =()=>(
-    <SmallMessageModel Button_Name="Bhavya" Modal_Title="ModalTitle" Modal_Message="Message About The Modal" />
+    <SmallMessageModel Button_Name="Open Modal" Modal_Title="ModalTitle" Modal_Message="Message About The Modal" />
 );
