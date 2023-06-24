4f69a9c6533825d8115ae24763995eaa4b5c7a47
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-06-24 23:19:39 +0530

    Landing Page Added

--- site/app/page.js
+++ site/app/page.js
@@ -1,7 +1,33 @@
+import Image from 'next/image';
+import blueIcon from '@/assets/blue.svg';
+import orangeIcon from '@/assets/orange.svg';
+import heap from '@/assets/heap.svg';
+
 export default function Home() {
+
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-center text-4xl">WEBSITE WILL BE UP SOON!....</h1>
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <div className='absolute bottom-0 left-0 h-[150px] w-[150px] md:h-[300px] md:w-[300px]'>
+        <Image src={blueIcon} alt="Blue Icon" />
+      </div>
+      <div className='absolute top-0 right-0 h-[150px] w-[150px] md:h-[300px] md:w-[250px]'>
+        <Image src={orangeIcon} alt="Blue Icon" />
+      </div>
+      <div className='absolute top-[100px] left-5 md:left-60'>
+        <Image src={heap} alt="heap" />
+      </div>
+      <div className='absolute top-[100px] right-5 md:right-80'>
+        <Image src={heap} alt="heap" />
+      </div>
+      <div className='absolute bottom-[10%] left-[15%] md:bottom-40 md:left-[30%]'>
+        <Image src={heap} alt="heap" />
+      </div>
+      <div className='flex items-center justify-center h-screen w-screen'>
+        <div className='flex flex-col items-start justify-center text-center pb-[5%] md:text-left'>
+          <h1 className='text-black px-2 font-pacifico text-[2.6rem] md:text-[4rem]'>Love Simple UI</h1>
+          <p className='px-2 text-black font-poppins text-[1.2rem] md:text-[1.5rem]'>A <span className='font-bold'>Lightweight</span> + <span className='font-bold'>Fast</span> + <span className='font-bold'>Responsive</span> open-source UI component library.</p>
+        </div>
+      </div>
     </main>
   );
 }
--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +12,10 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        pacifico: ['Pacifico', 'cursive'],
+        poppins: ['Poppins', 'sans-serif'],
       },
     },
   },
