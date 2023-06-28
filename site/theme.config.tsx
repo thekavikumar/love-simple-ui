7d2a3cb77cd905d66533ab82bbbbba4eb0329df1
thekavikumar <kavikumarceo@gmail.com>
2023-06-28 22:39:31 +0530

    site error resolved

--- site/theme.config.tsx
+++ site/theme.config.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
-import "./components/logo.module.css";
 
 const config: DocsThemeConfig = {
-  logo: <h1 className="logo">love simple ui </h1>,
+  logo: <h1>love simple ui </h1>,
   project: {
     link: "https://github.com/thekavikumar/love-simple-ui",
   },
