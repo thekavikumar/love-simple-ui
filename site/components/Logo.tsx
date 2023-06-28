34dec6c04c9dc8a3f3c4f594f48b6b9e3daae45b
thekavikumar <kavikumarceo@gmail.com>
2023-06-28 22:38:43 +0530

    site error resolved

--- site/components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./logo.module.css"
-
-function Logo() {
-  return <h1 className="logo">love simple ui</h1>;
-}
-
-export default Logo;
--- site/components/counters.tsx
+++ site/components/counters.tsx
@@ -1,24 +1,21 @@
 // Example from https://beta.reactjs.org/learn
 
-import { useState } from 'react'
-import styles from './counters.module.css'
+import { useState } from "react";
 
 function MyButton() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   function handleClick() {
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
   return (
     <div>
-      <button onClick={handleClick} className={styles.counter}>
-        Clicked {count} times
-      </button>
+      <button onClick={handleClick}>Clicked {count} times</button>
     </div>
-  )
+  );
 }
 
 export default function MyApp() {
-  return <MyButton />
+  return <MyButton />;
 }
