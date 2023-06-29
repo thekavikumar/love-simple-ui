cf03a2cfe1b7bce746a3a43896fd91837992811b
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2023-06-29 22:48:06 +0530

    Update infinitescroll.stories.jsx

--- src/InfiniteScroll/infinitescroll.stories.jsx
+++ src/InfiniteScroll/infinitescroll.stories.jsx
@@ -4,8 +4,14 @@ import InfiniteScroll from './infinitescroll';
 export default {
   title: 'Components/InfiniteScroll',
   component: InfiniteScroll,
+  argTypes: {
+    apiUrl: { control: 'text' },
+  },
 };
 
-const Template = () => <InfiniteScroll />;
+const Template = (args) => <InfiniteScroll {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  apiUrl: 'https://api.example.com/data',
+};
