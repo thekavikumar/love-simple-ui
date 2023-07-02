6c58118d8e4af59dc0320e1f3bbca81145c5b4a4
Pratham Mahajan <111378171+Killer2OP@users.noreply.github.com>
2023-07-02 23:20:34 +0530

    Update Button.stories.tsx

--- src/buttons/Button.stories.tsx
+++ src/buttons/Button.stories.tsx
@@ -14,38 +14,38 @@ import {
   MasterCardButton,
 } from "./index";
 
-export const ButtonOne = () => <Button1>Hello World!</Button1>; // Button one demo
+export const ButtonOne = () => <Button1 role="button" name="Button 1"className="Button 1">Hello World!</Button1>; // Button one demo
 
-export const ButtonTwo = () => <Button2 className="">Hello World!</Button2>; // Button two demo
+export const ButtonTwo = () => <Button2 role="button" name="Button 2" className="">Hello World!</Button2>; // Button two demo
 
-export const ButtonThree = () => <Button3 className="">Hello World!</Button3>; // Button three demo
+export const ButtonThree = () => <Button3 role="button" name="Button 3" className="">Hello World!</Button3>; // Button three demo
 
-export const ButtonFour = () => <Button4 className="">Hello World!</Button4>; // Button four demo
+export const ButtonFour = () => <Button4 role="button" name="Button 4" className="">Hello World!</Button4>; // Button four demo
 
-export const ButtonFive = () => <Button5 className="">Hello World!</Button5>; // Button five demo
+export const ButtonFive = () => <Button5 role="button" name="Button 5" className="">Hello World!</Button5>; // Button five demo
 
-export const ButtonSix = () => <Button6 className="">Hello World!</Button6>; // Button six demo
+export const ButtonSix = () => <Button6 role="button" name="Button 6" className="">Hello World!</Button6>; // Button six demo
 
 export const FaceBookBtn = () => (
-  <FaceBookButton className=""/>
+  <FaceBookButton role="button" name="facebook-button" className=""/>
 ); // FaceBookButton demo
 
 export const TwitterBtn = () => (
-  <TwitterButton className=""/>
+  <TwitterButton role="button" name="twitter-button" className=""/>
 ); // TwitterButton demo
 
 export const GithubBtn = () => (
-  <GithubButton className=""/>
+  <GithubButton role="button" name="github-button" className=""/>
 ); // GithubButton demo
 
 export const GoogleBtn = () => (
-  <GoogleButton className=""/>
+  <GoogleButton role="button" name="google-button" className=""/>
 ); // GoogleButton demo
 
 export const PayPalBtn = () => (
-  <PayPalButton className=""/>
+  <PayPalButton role="button" name="paypal-button" className=""/>
 ); // PayPalButton demo
 
 export const MasterCardBtn = () => (
-  <MasterCardButton className=""/>
+  <MasterCardButton role="button" name="mastercard-button" className=""/>
 ); // MasterCardButton demo
