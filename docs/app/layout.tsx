90ef7e9b63c9ac52815046cfec50a3f664d1310c
Kavikumar M <kavikumarceo@gmail.com>
2024-12-30 13:31:41 +0530

    updated the docs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { ThemeProvider } from "@/components/contexts/theme-provider";
-import { Navbar } from "@/components/navbar";
-import { Space_Mono, Space_Grotesk } from "next/font/google";
-import { Footer } from "@/components/footer";
-import "@/styles/globals.css";
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/components/contexts/theme-provider';
+import { Navbar } from '@/components/navbar';
+import { Space_Mono, Space_Grotesk } from 'next/font/google';
+import { Footer } from '@/components/footer';
+import '@/styles/globals.css';
 
 const sansFont = Space_Grotesk({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-  display: "swap",
-  weight: "400",
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+  display: 'swap',
+  weight: '400',
 });
 
 const monoFont = Space_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-  display: "swap",
-  weight: "400",
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+  display: 'swap',
+  weight: '400',
 });
 
 export const metadata: Metadata = {
@@ -39,7 +39,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
