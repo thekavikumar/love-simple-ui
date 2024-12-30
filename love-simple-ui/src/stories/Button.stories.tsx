eb89d0cdf27e2672255761db97f834f64a3ab561
Kavikumar M <kavikumarceo@gmail.com>
2024-12-30 10:25:31 +0530

    story

--- love-simple-ui/src/stories/Button.stories.tsx
+++ love-simple-ui/src/stories/Button.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../components/Button';
-import '../index.css';
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
